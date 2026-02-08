a979b2ee969a46db6e8c10c669ada2c503c38342
Daniel <danielschillerclimaco@gmail.com>
2026-02-08 20:47:32 +0000

    add validation testing

---
+++ packages/server/src/validation.ts
@@ -0,0 +1,55 @@
+type Val<T> =
+    (
+        T extends string ?
+        SeamValueString :
+        T extends number ?
+        SeamValueNumber :
+        T extends any[] ?
+        SeamValueArray<Unpacked<T>> :
+        { [K in keyof T]: Val<T[K]> }
+    ) & {
+        custom(): Val<T>
+    };
+
+interface SeamValueString {
+    min(value: number): SeamValueString;
+    max(value: number): SeamValueString;
+    length(value: number): SeamValueString;
+    startsWith(value: string): SeamValueString;
+    endsWith(value: string): SeamValueString;
+    includes(value: string): SeamValueString;
+    regex(regExpr: string | RegExp): SeamValueString;
+    email(): SeamValueString;
+}
+
+interface SeamValueNumber {
+    gt(value: number): SeamValueNumber;
+    gte(value: number): SeamValueNumber;
+    lt(value: number): SeamValueNumber;
+    lte(value: number): SeamValueNumber;
+    integer(): SeamValueNumber;
+}
+
+interface SeamValueArray<T> {
+    min(value: number): SeamValueArray<T>;
+    max(value: number): SeamValueArray<T>;
+    length(value: number): SeamValueArray<T>;
+    every(filter: Val<T>): SeamValueArray<T>;
+    some(filter: Val<T>): SeamValueArray<T>;
+}
+
+type Unpacked<T> = T extends (infer U)[] ? U : T;
+
+
+interface UserData {
+    description?: string;
+    metadata: {
+        id: number;
+    }
+}
+
+function createUser(name: Val<string>, age: Val<number>, data: Val<UserData>) {
+    name.min(3).max(50);
+    age.gte(1).lt(150);
+    data.description?.max(250);
+}
